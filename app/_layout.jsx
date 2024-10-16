dcd7ca445a4df63d59dbb501ba2d92a6fa4f661d
jonah.delrosario <delrosariojonahagliam@gmail.com>
2024-10-17 03:05:23 +0800

    latest

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -9,15 +9,15 @@ SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
-    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
-    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
-    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
-    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
-    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
-    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
-    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
-    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
-    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-Black": require("../assets/fonts/Outfit-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Outfit-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Outfit-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Outfit-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Outfit-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Outfit-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Outfit-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Outfit-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Outfit-Thin.ttf"),
   });
 
   useEffect(() => {
--- constants/images.js
+++ constants/images.js
@@ -1,23 +1,9 @@
-import profile from "../assets/images/profile.png";
-import thumbnail from "../assets/images/thumbnail.png";
-import cards from "../assets/images/cards.png";
-import path from "../assets/images/path.png";
-import logo from "../assets/images/logo.png";
-import logoSmall from "../assets/images/logo-small.png";
-import empty from "../assets/images/empty.png";
 import splash from "../assets/images/splash.png";
 import perksLogo from "../assets/images/perks-logo.png";
 import perksIcon from "../assets/images/perks-icon.png";
 
-export default { 
-    profile, 
-    thumbnail, 
-    cards, 
-    path, 
-    logo, 
-    logoSmall, 
-    empty, 
-    splash, 
+export default {
+    splash,
     perksLogo,
-    perksIcon 
+    perksIcon
 };
